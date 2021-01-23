427dbf2fc14465b9ca1941819540a3fb476d0f04
Milo <50248166+Milo123459@users.noreply.github.com>
2021-01-23 18:35:28 +0000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-declare function createBar(total: number, current: number, size?:number, line?: string, slider?: string): string
+declare function createBar(total: number, current: number, size?:number, line?: string, slider?: string): Array<string>
 
 export default createBar;
